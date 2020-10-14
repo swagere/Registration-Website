21d820e344addd9058ac84ea43bd9d4d30f96667
swagere <1250539180@qq.com>
2020-10-14 17:50:52 +0800

    init

---
+++ src/main/java/rpz/registrationwebsite/RegistrationWebsiteApplication.java
@@ -0,0 +1,13 @@
+package rpz.registrationwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistrationWebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rpz/registrationwebsite/RegistrationWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package rpz.registrationwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
